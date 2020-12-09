d670fdc937e17fb4dbe9b805eb39a84cdf3b2eb9
Igor Cazé Nunes <igorcaze@gmail.com>
2020-12-09 10:42:59 -0300

    Feat: Criação de repositório

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const {
+	uuid,
+	isUuid
+} = require('uuid');
 
 const app = express();
 
@@ -11,23 +14,41 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  return response.json(repositories);
+	return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+	const {
+		title,
+		url,
+		techs
+	} = request.body;
+
+	const repository = {
+		id: uuid(),
+		title,
+		url,
+		techs,
+		likes: 0
+	}
+
+	repositories.push(repository);
+
+	return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+	// TODO
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+	// TODO
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+	// TODO
 });
 
 module.exports = app;
